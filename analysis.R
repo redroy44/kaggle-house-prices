387de0e29f26311cd9dc4b9be17ae6fa6f30ca0e
Piotr Bandurski <redroy44@gmail.com>
2016-12-11 01:08:06 +0100

    explored nearZeroVar and missing values columns

--- analysis.R
+++ analysis.R
@@ -2,7 +2,8 @@
 library(checkpoint)
 checkpoint("2016-11-15")
 library(tidyverse)
-
+library(forcats)
+library(caret)
 library(doMC)
 registerDoMC(cores = 8)
 set.seed(29082012)
@@ -13,14 +14,41 @@ file_test <- "data/test.csv"
 
 
 # Load the train & test dataset -------------------------------------------
-column_types <- cols_only(a = col_factor())
+column_types <- cols_only()
 
-train_data <- read_csv(file_train, col_types = column_types)
-test_data <- read_csv(file_test, col_types = column_types)
+train_data <- read_csv(file_train)
+test_data <- read_csv(file_test)
 
 full_data = bind_rows(train_data, test_data)
 
+# Check character columns and turn them into factors-----------------------
+full_data %>%  
+  select_if(is_character) %>%
+  summarise_each(funs(n_distinct(.))) %>%
+  t() %>% data.frame() %>% as_tibble() %>% rownames_to_column() %>%
+  arrange(desc(.))
+
+full_data <- full_data %>%
+  mutate_if(is_character, funs(as.factor))
+
+glimpse(full_data)
+summary(full_data)
 # Look for NA's in full_data ----------------------------------------------
 full_data %>%
   summarise_each(funs(sum(is.na(.)))) %>%
-  print
+  t() %>% data.frame() %>% as_tibble() %>% rownames_to_column() %>%
+  mutate(percent = ./nrow(full_data)*100) %>%
+  arrange(desc(percent))
+
+# Explore columns with small variance
+full_data %>%
+  select(nearZeroVar(.))
+
+
+
+
+
+
+
+
+
